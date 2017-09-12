6f1260314ba71557e809398e8a22551689947bf5
Gladysgong <531404376@qq.com>
2017-09-12 17:51:06 +0800

    全连接神经网络

--- learn_dl/bp.py
+++ learn_dl/bp.py
@@ -8,6 +8,7 @@ def sigmoid(inX):
     return 1.0 / (1 + exp(-inX))
 
 
+# Node 节点对象计算和记录节点自身的信息(比如输出值、误差项等)，以及与这个节点相关的上下游的连接。
 # 节点类，负责记录和维护节点自身信息以及与这个节点相关的上下游连接，实现输出值和误差项的计算。
 class Node(object):
     def __init__(self, layer_index, node_index):
@@ -38,7 +39,7 @@ class Node(object):
     def calc_outppupt(self):
         '''
         根据式1计算节点的输出
-        y=sigmoid(wx) 式1
+        y=sigmoid(wx) (式1)
         '''
         output = reduce(lambda ret, conn: ret + conn.upstream_node.output * conn.weight, self.upstream, 0)
         self.output = sigmoid(output)
@@ -69,6 +70,7 @@ class Node(object):
         return node_str + '\n\tdownstream:' + downstream_str + '\n\tupstream' + upstream_str
 
 
+# ConstNode对象，为了实现一个输出恒为1的节点(计算偏置项时需要)
 class ConstNode(object):
     def __init__(self, layer_index, node_index):
         '''
@@ -101,20 +103,23 @@ class ConstNode(object):
         downstream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.downstream, '')
         return node_str + '\n\tdownstream:' + downstream_str
 
+
+# Layer层对象，由多个节点组成。
+# Layer对象，负责初始化一层。此外，作为Node的集合对象，提供对Node集合的操作。
 class Layer(object):
-    def __init__(self,layer_index,node_count):
+    def __init__(self, layer_index, node_count):
         '''
         初始化一层
         :param layer_index: 层编号
         :param node_count: 层所包含的节点个数
         '''
-        self.layer_index=layer_index
-        self.nodes=[]
+        self.layer_index = layer_index
+        self.nodes = []
         for i in range(node_count):
-            self.nodes.append(Node(layer_index,i))
-        self.nodes.append(ConstNode(layer_index,node_count))
+            self.nodes.append(Node(layer_index, i))
+        self.nodes.append(ConstNode(layer_index, node_count))
 
-    def set_output(self,data):
+    def set_output(self, data):
         '''设置层的输出，当层时输入层时会用到。'''
         for i in range(len(data)):
             self.nodes[i].set_output(data[i])
@@ -123,35 +128,40 @@ class Layer(object):
         '''计算层的输出向量'''
         for node in self.nodes[:-1]:
             node.calc_output()
+
     def dump(self):
         '''打印的信息'''
         for node in self.nodes:
             print node
 
-# 职责：记录连接的权重，以及这个连接所关联的上下游节点
+
+# Connection每个连接对象都要记录该连接的权重。
+# Connection对象，主要职责是记录连接的权重，以及这个连接所关联的上下游节点。
 class Connection(object):
-    def __init__(self,upstream_node,downstream_node):
+    def __init__(self, upstream_node, downstream_node):
         '''
         初始化连接，权重初始化为一个很小的随机数
         :param upstream_node: 连接的上游节点
         :param downstream_node: 连接的下游节点
         '''
-        self.upstream_node=upstream_node
-        self.downstream_node=downstream_node
-        self.weight=random.uniform(-0.1,0.1)
-        self.gradient=0.0
+        self.upstream_node = upstream_node
+        self.downstream_node = downstream_node
+        self.weight = random.uniform(-0.1, 0.1)
+        self.gradient = 0.0
 
     def calc_gradient(self):
         '''计算梯度'''
-        self.gradient=self.downstream_node.delta*self.upstream_node.output
+        self.gradient = self.downstream_node.delta * self.upstream_node.output  # δx
 
     def get_gradient(self):
         '''获取当前的梯度'''
         return self.gradient
-    def update_weight(self,rate):
+
+    def update_weight(self, rate):
         '''根据梯度下降算法更新权重'''
         self.calc_gradient()
-        self.weight+=rate*self.gradient
+        self.weight += rate * self.gradient  # w<--w+nδx
+
     def __str__(self):
         '''打印连接信息'''
         return '(%u-%u) -> (%u-%u) = %f' % (
@@ -161,25 +171,57 @@ class Connection(object):
             self.downstream_node.node_index,
             self.weight)
 
-# 提供Connection集合操作
+
+# Connections 仅仅作为Connection的集合对象，提供一些集合操作。
+# Connections对象，提供Connection集合操作。
 class Connections(object):
     def __init__(self):
-        self.connections=[]
-    def add_connections(self,connection):
+        self.connections = []
+
+    def add_connections(self, connection):
         self.connections.append(connection)
+
     def dump(self):
         for conn in self.connections:
             print conn
 
-# 提供API
+
+# Network 神经网络对象，提供API接口。它由若干层对象组成以及连接对象组成。
 class Network(object):
-    def __init__(self,layers):
+    def __init__(self, layers):
         '''
         初始化一个全连接神经网络
         :param layers: 二维数组，描述神经网络每层节点数
         '''
-        self.connections=Connections()
-        self.layers=[]
-        layer_conut=len(layers)
-        node_count=0
-        for i in range(layers)
+        self.connections = Connections()
+        self.layers = []
+        layer_count = len(layers)
+        node_count = 0
+        for i in range(layer_count):
+            self.layers.append(Layer(i, layers[i]))
+        for layer in range(layer_count-1):
+            connections=[Connection(upstream_node,downstream_node)
+                         for upstream_node in self.layers[layer].nodes
+                         for downstream_node in self.layers[layer+1].nodes[:-1]]
+            for conn in connections:
+                self.connections.add_connections(conn)
+                conn.downstream_node.append_upstream_connection(conn)
+                conn.upstream_node.append_downstream_connection(conn)
+
+    def train(self,labels,data_set,rate,iteration):
+        '''
+        训练神经网络
+        :param labels:数组，训练样本标签。每个元素是一个样本的标签。
+        :param data_set:二维数组，训练样本特征。每个元素是一个样本的特征。
+        :param rate:
+        :param iteration:
+        :return:
+        '''
+        for i in range(iteration):
+            for d in range(len(data_set)):
+                self.train()
+
+
+    def train_one_sample(self,label,sample,rate):
+        '''内部函数，用一个样本训练网络'''
+        pass
